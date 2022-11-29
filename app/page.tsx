804187a9eafcc528ec3bdb5a30d8be192143bf6f
LaouiniSofiene <sofiene.laouini@esprit.tn>
2022-11-29 12:41:46 +0100

    Project init
    **Reservation App init
    
    *Stack :
      NextJS 13.0.5
      React 18.2.0
      TailwindCSS 3.2.4
    
    *Project structure :
    NextJs 13 structure ('App folder instead of Pages folder )

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+
+function homePage() {
+  return (
+    <div>
+        <h1 className="text-5xl font-bold">
+            Reservation App
+        </h1>
+    </div>
+  )
+}
+
+export default homePage
